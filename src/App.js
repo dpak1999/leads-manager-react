f87bab94863990b3ef07e6afa5927e4cc38d5485
deepak <deepakkumardash313@gmail.com>
2022-04-22 00:30:36 +0530

    setup material ui theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 /** @format */
 
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import { memo } from 'react';
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography>Leads Manager</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <h1>Hello world</h1>
+      <Button variant="contained" color="primary">
+        Test button
+      </Button>
+      <Typography color="warning.main">Test typography</Typography>
+    </>
+  );
 }
 
 export default memo(App);
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import { createTheme } from '@mui/material/styles';
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#00695c',
+    },
+    warning: {
+      main: '#ef6c00',
+    },
+    success: {
+      main: '#2e7d32',
+    },
+  },
+});
